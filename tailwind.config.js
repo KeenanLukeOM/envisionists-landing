6391dc5539f73c67b4c121a2082a62b9c5dc35d4
Keenan <dj.kflux@gmail.com>
2023-06-07 17:11:13 -0700

    Spacing, footer, padding changes, comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,8 @@ module.exports = {
       colors: {
         primary: {
           1: "#1D0707",
+          11: "#180707",
+          111: "#120707",
           2: "#7c68c0",
           3: "#e9e6f4",
           4: "#8c7ac7",
@@ -28,6 +30,24 @@ module.exports = {
         TheNeue: ["TheNeue", "Arial", "sans-serif"],
         Inter: ["Inter", "Arial", "sans-serif"],
       },
+    },
+    screens: {
+      sm: "360px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "640px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1280px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1920px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "2560px",
+      // => @media (min-width: 1536px) { ... }
+
+      "3xl": "3840px",
     },
   },
   mode: "jit",
